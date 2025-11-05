2f9fc59b9114e78e64087255971cdfb9b483b5d3
Mirko Lana <lana.mirko@icloud.com>
2025-11-05 18:10:43 +0100

    chore: merge 'dev' brench for new error message handling and better documentation

--- firmware/include/fsm.h
+++ firmware/include/fsm.h
@@ -11,32 +11,46 @@
 #ifndef FSM_H
 #define FSM_H
 
+/*!
+ * \brief           An enumeration with all the possible FSM states.
+ */
 enum FsmState {
-    FSM_STATE_INIT,
-    FSM_STATE_WIFI_CONNECTION,
-    FSM_STATE_CHECK_CONFIG,
-    FSM_STATE_LOAD_CONFIG,
-    FSM_STATE_DISPLAY_UPDATE,
-    FSM_STATE_ERROR,
-    FSM_STATE_LEN
+    FSM_STATE_INIT,            /*!< Initialization state */
+    FSM_STATE_WIFI_CONNECTION, /*!< Connecting to WiFi network */
+    FSM_STATE_CHECK_CONFIG,    /*!< Checking configuration presence or "version" */
+    FSM_STATE_LOAD_CONFIG,     /*!< Loading/fetching configuration */
+    FSM_STATE_DISPLAY_UPDATE,  /*!< Updating the display and relative models */
+    FSM_STATE_ERROR,           /*!< Error state */
+    FSM_STATE_LEN              /*!< Length of the enum, not a real state */
 };
 
+/*!
+ * \brief           Initialize the FSM.
+ */
 void fsm_init(void);
 
 /*!
- * \brief           get the current state of the FSM.
+ * \brief           Get the current state of the FSM.
  *
- * \return          state of the FSM
+ * \return          The state of the FSM.
  */
 enum FsmState fsm_get_state(void);
 
-const char *fsm_get_state_name(void);
+/*!
+ * \brief           Get the given state name.
+ *
+ * \param[in]       state: The state to get the name for.
+ * \return          The name of the given state.
+ */
+const char *fsm_get_state_name(enum FsmState state);
 
 /*!
- * \brief           update the current state of the FSM.
+ * \brief           Update the current state of the FSM.
  *
- * \param[out]      next_state: The next FSM state.           
- * \return          RC_OK: state of the FSM
+ * \param[out]      next_state: The next FSM state.
+ * \return          RC_OK on success, an error code otherwise:
+ *                   - RC_FSM_ERR_INVALID_NEXT_STATE  if the given state is not
+ *                     relat  to the current one.
  */
 int fsm_update(enum FsmState next_state);
 
--- firmware/src/fsm.c
+++ firmware/src/fsm.c
@@ -1,13 +1,31 @@
 #include "rc.h"
 #include "fsm.h"
 
+/*!
+ * \brief           Type definition for FSM transition functions.
+ *
+ * \param[in]       next_state: The next FSM state.
+ * \return          RC_OK on success, an error code otherwise:
+ *                   - RC_FSM_ERR_INVALID_NEXT_STATE if the given state is not
+ *                     related to the current one.
+ */
 typedef int (*fsm_trans_fun)(enum FsmState next_state);
 
-static const char *FSM_STATE_NAMES[FSM_STATE_LEN] = { "FSM_STATE_INIT", "FSM_STATE_WIFI_CONNECTION", "FSM_STATE_CHECK_CONFIG", "FSM_STATE_LOAD_CONFIG", "FSM_STATE_DISPLAY_UPDATE", "FSM_STATE_ERROR", "FSM_STATE_LEN" };
+static const char *FSM_STATE_NAMES[FSM_STATE_LEN] = {
+    "FSM_STATE_INIT",
+    "FSM_STATE_WIFI_CONNECTION",
+    "FSM_STATE_CHECK_CONFIG",
+    "FSM_STATE_LOAD_CONFIG",
+    "FSM_STATE_DISPLAY_UPDATE",
+    "FSM_STATE_ERROR"
+}; /*!< Array of state names for debugging purposes */
 
-static enum FsmState fsm_current_state;
-static fsm_trans_fun fsm_trans_array[FSM_STATE_LEN];
+static enum FsmState fsm_current_state;              /*!< Current FSM state */
+static fsm_trans_fun fsm_trans_array[FSM_STATE_LEN]; /*!< Array of transition functions */
 
+/*!
+ * \brief           Transition function for the INIT state.
+ */
 static int prv_fsm_init_trans_fun(enum FsmState next_state) {
     switch (next_state) {
         case FSM_STATE_WIFI_CONNECTION:
@@ -21,6 +39,9 @@ static int prv_fsm_init_trans_fun(enum FsmState next_state) {
     return RC_OK;
 }
 
+/*!
+ * \brief           Transition function for the WIFI_CONNECTION state.
+ */
 static int prv_fsm_wifi_connection_trans_fun(enum FsmState next_state) {
     switch (next_state) {
         case FSM_STATE_CHECK_CONFIG:
@@ -35,6 +56,9 @@ static int prv_fsm_wifi_connection_trans_fun(enum FsmState next_state) {
     return RC_OK;
 }
 
+/*!
+ * \brief           Transition function for the CHECK_CONFIG state.
+ */
 static int prv_fsm_check_config_trans_fun(enum FsmState next_state) {
     switch (next_state) {
         case FSM_STATE_ERROR:
@@ -48,6 +72,9 @@ static int prv_fsm_check_config_trans_fun(enum FsmState next_state) {
     return RC_OK;
 }
 
+/*!
+ * \brief           Transition function for the LOAD_CONFIG state.
+ */
 static int prv_fsm_load_config_trans_fun(enum FsmState next_state) {
     switch (next_state) {
         case FSM_STATE_ERROR:
@@ -61,6 +88,9 @@ static int prv_fsm_load_config_trans_fun(enum FsmState next_state) {
     return RC_OK;
 }
 
+/*!
+ * \brief           Transition function for the DISPLAY_UPDATE state.
+ */
 static int prv_fsm_display_update_trans_fun(enum FsmState next_state) {
     switch (next_state) {
         case FSM_STATE_ERROR:
@@ -75,6 +105,9 @@ static int prv_fsm_display_update_trans_fun(enum FsmState next_state) {
     return RC_OK;
 }
 
+/*!
+ * \brief           Transition function for the ERROR state.
+ */
 static int prv_fsm_error_trans_fun(enum FsmState next_state) {
     switch (next_state) {
         case FSM_STATE_ERROR:
@@ -103,8 +136,8 @@ enum FsmState fsm_get_state(void) {
     return fsm_current_state;
 }
 
-const char *fsm_get_state_name(void) {
-    return FSM_STATE_NAMES[fsm_current_state];
+const char *fsm_get_state_name(enum FsmState state) {
+    return FSM_STATE_NAMES[state];
 }
 
 int fsm_update(enum FsmState next_state) {
