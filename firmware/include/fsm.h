bd693487c05006d20dcc865235607aaff52c9d91
Mirko Lana <lana.mirko@icloud.com>
2025-11-05 10:37:15 +0100

    chore: add fms.c/h files

---
+++ firmware/include/fsm.h
@@ -0,0 +1,14 @@
+/*!
+ * \file            fsm.h
+ * \date            2025-10-27
+ * \author          Giulia Cristofolini [giulia.cristofolini@studenti.unitn.it]
+ *                  Mirko Lana [lana.mirko@icloud.com]
+ *                  Mattia Zagatti [mattia.zagatti@studenti.unitn.it]
+ *
+ * \brief           FSM Module.
+ */
+
+#ifndef FSM_H
+#define FSM_H
+
+#endif /*! FSM_H */
