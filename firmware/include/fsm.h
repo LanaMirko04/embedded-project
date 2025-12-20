b380f5b44d5e41b77aae3bd6645d59a9c4f42836
Mirko Lana <lana.mirko@icloud.com>
2025-12-20 15:17:10 +0100

    feat: merge `feature-cpp-error-handling-refactor` into `dev`

---
+++ firmware/include/fsm.h
@@ -0,0 +1,59 @@
+/*!
+ * \file            fsm.h
+ * \date            2025-10-27
+ * \author          Giulia Cristofolini [giulia.cristofolini@studenti.unitn.it]
+ *                  Mirko Lana [lana.mirko@icloud.com]
+ *                  Mattia Zagatti [mattia.zagatti@studenti.unitn.it]
+ *
+ * \brief           FSM Module.
+ */
+
+#ifndef FSM_H
+#define FSM_H
+
+#include "result.h"
+
+/*!
+ * \brief           An enumeration with all the possible FSM states.
+ */
+enum FsmState {
+    FSM_STATE_INIT,            /*!< Initialization state */
+    FSM_STATE_WIFI_CONNECTION, /*!< Connecting to WiFi network */
+    FSM_STATE_CHECK_CONFIG,    /*!< Checking configuration presence or "version" */
+    FSM_STATE_LOAD_CONFIG,     /*!< Loading/fetching configuration */
+    FSM_STATE_DISPLAY_UPDATE,  /*!< Updating the display and relative models */
+    FSM_STATE_ERROR,           /*!< Error state */
+    FSM_STATE_LEN              /*!< Length of the enum, not a real state */
+};
+
+/*!
+ * \brief           Initialize the FSM.
+ */
+void fsm_init(void);
+
+/*!
+ * \brief           Get the current state of the FSM.
+ *
+ * \return          The state of the FSM.
+ */
+enum FsmState fsm_get_state(void);
+
+/*!
+ * \brief           Get the given state name.
+ *
+ * \param[in]       state: The state to get the name for.
+ * \return          The name of the given state.
+ */
+const char *fsm_get_state_name(enum FsmState state);
+
+/*!
+ * \brief           Update the current state of the FSM.
+ *
+ * \param[out]      next_state: The next FSM state.
+ * \return          RC_OK on success, an error code otherwise:
+ *                   - RC_FSM_ERR_INVALID_NEXT_STATE  if the given state is not
+ *                     relat  to the current one.
+ */
+Result fsm_update(enum FsmState next_state);
+
+#endif /*! FSM_H */
---
+++ firmware/src/fsm.cpp
@@ -0,0 +1,165 @@
+#include "fsm.h"
+#include "result.h"
+
+/*!
+ * \brief           Type definition for FSM transition functions.
+ *
+ * \param[in]       next_state: The next FSM state.
+ * \return          Result::SUCCESS on success, an error code otherwise:
+ *                   - Result::INVALID_NEXT_STATE if the given state is not
+ *                     related to the current one.
+ */
+typedef Result (*fsm_trans_fun)(enum FsmState next_state);
+
+static const char *FSM_STATE_NAMES[FSM_STATE_LEN] = {
+    "FSM_STATE_INIT",
+    "FSM_STATE_WIFI_CONNECTION",
+    "FSM_STATE_CHECK_CONFIG",
+    "FSM_STATE_LOAD_CONFIG",
+    "FSM_STATE_DISPLAY_UPDATE",
+    "FSM_STATE_ERROR"
+}; /*!< Array of state names for debugging purposes */
+
+static enum FsmState fsm_current_state;              /*!< Current FSM state */
+static fsm_trans_fun fsm_trans_array[FSM_STATE_LEN]; /*!< Array of transition functions */
+
+/*!
+ * \brief           Transition function for the INIT state.
+ */
+static Result prv_fsm_init_trans_fun(enum FsmState next_state) {
+    switch (next_state) {
+        case FSM_STATE_INIT:
+        case FSM_STATE_ERROR:
+        case FSM_STATE_WIFI_CONNECTION:
+        case FSM_STATE_CHECK_CONFIG:
+            fsm_current_state = next_state;
+            break;
+        default:
+
+            result_set_err_msg("The state %s is not reachable from %s state.",
+                           fsm_get_state_name(next_state),
+                           fsm_get_state_name(fsm_current_state));
+            return Result::INVALID_NEXT_STATE;
+    }
+    return Result::SUCCESS;
+}
+
+/*!
+ * \brief           Transition function for the WIFI_CONNECTION state.
+ */
+static Result prv_fsm_wifi_connection_trans_fun(enum FsmState next_state) {
+    switch (next_state) {
+        case FSM_STATE_CHECK_CONFIG:
+        case FSM_STATE_WIFI_CONNECTION:
+        case FSM_STATE_ERROR:
+            fsm_current_state = next_state;
+            break;
+        default:
+
+            result_set_err_msg("The state %s is not reachable from %s state.",
+                           fsm_get_state_name(next_state),
+                           fsm_get_state_name(fsm_current_state));
+            return Result::INVALID_NEXT_STATE;
+    }
+    return Result::SUCCESS;
+}
+
+/*!
+ * \brief           Transition function for the CHECK_CONFIG state.
+ */
+static Result prv_fsm_check_config_trans_fun(enum FsmState next_state) {
+    switch (next_state) {
+        case FSM_STATE_ERROR:
+        case FSM_STATE_LOAD_CONFIG:
+            fsm_current_state = next_state;
+            break;
+        default:
+
+            result_set_err_msg("The state %s is not reachable from %s state.",
+                           fsm_get_state_name(next_state),
+                           fsm_get_state_name(fsm_current_state));
+            return Result::INVALID_NEXT_STATE;
+    }
+    return Result::SUCCESS;
+}
+
+/*!
+ * \brief           Transition function for the LOAD_CONFIG state.
+ */
+static Result prv_fsm_load_config_trans_fun(enum FsmState next_state) {
+    switch (next_state) {
+        case FSM_STATE_ERROR:
+        case FSM_STATE_DISPLAY_UPDATE:
+            fsm_current_state = next_state;
+            break;
+
+        default:
+            result_set_err_msg("The state %s is not reachable from %s state.",
+                           fsm_get_state_name(next_state),
+                           fsm_get_state_name(fsm_current_state));
+            return Result::INVALID_NEXT_STATE;
+    }
+    return Result::SUCCESS;
+}
+
+/*!
+ * \brief           Transition function for the DISPLAY_UPDATE state.
+ */
+static Result prv_fsm_display_update_trans_fun(enum FsmState next_state) {
+    switch (next_state) {
+        case FSM_STATE_ERROR:
+        case FSM_STATE_DISPLAY_UPDATE:
+        case FSM_STATE_INIT:
+            fsm_current_state = next_state;
+            break;
+
+        default:
+            result_set_err_msg("The state %s is not reachable from %s state.",
+                           fsm_get_state_name(next_state),
+                           fsm_get_state_name(fsm_current_state));
+            return Result::INVALID_NEXT_STATE;
+    }
+    return Result::SUCCESS;
+}
+
+/*!
+ * \brief           Transition function for the ERROR state.
+ */
+static Result prv_fsm_error_trans_fun(enum FsmState next_state) {
+    switch (next_state) {
+        case FSM_STATE_ERROR:
+        case FSM_STATE_INIT:
+            fsm_current_state = next_state;
+            break;
+
+        default:
+            result_set_err_msg("The state %s is not reachable from %s state.",
+                           fsm_get_state_name(next_state),
+                           fsm_get_state_name(fsm_current_state));
+            return Result::INVALID_NEXT_STATE;
+    }
+    return Result::SUCCESS;
+}
+
+void fsm_init(void) {
+    fsm_current_state = FSM_STATE_INIT;
+
+    fsm_trans_array[FSM_STATE_INIT] = prv_fsm_init_trans_fun;
+    fsm_trans_array[FSM_STATE_WIFI_CONNECTION] = prv_fsm_wifi_connection_trans_fun;
+    fsm_trans_array[FSM_STATE_CHECK_CONFIG] = prv_fsm_check_config_trans_fun;
+    fsm_trans_array[FSM_STATE_LOAD_CONFIG] = prv_fsm_load_config_trans_fun;
+    fsm_trans_array[FSM_STATE_DISPLAY_UPDATE] = prv_fsm_display_update_trans_fun;
+    fsm_trans_array[FSM_STATE_ERROR] = prv_fsm_error_trans_fun;
+}
+
+enum FsmState fsm_get_state(void) {
+    return fsm_current_state;
+}
+
+const char *fsm_get_state_name(enum FsmState state) {
+    return FSM_STATE_NAMES[state];
+}
+
+Result fsm_update(enum FsmState next_state) {
+    return fsm_trans_array[fsm_current_state](next_state);
+}
