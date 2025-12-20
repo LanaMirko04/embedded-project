4fc67706dc4aa003ca53140cb8219979c4aebb5a
Mirko Lana <lana.mirko@icloud.com>
2025-12-20 13:55:21 +0100

    feat: error handling refactor

---
+++ firmware/include/result.h
@@ -0,0 +1,69 @@
+/*!
+ * \file            result.h
+ * \date            2025-12-19
+ * \author          Giulia Cristofolini [giulia.cristofolini@studenti.unitn.it]
+ *                  Mirko Lana [lana.mirko@icloud.com]
+ *                  Mattia Zagatti [mattia.zagatti@studenti.unitn.it]
+ *
+ * \brief           Return codes and error handling.
+ */
+
+#ifndef RESULT_H
+#define RESULT_H
+
+#include <cstddef>
+
+/*!
+ * \brief           Error message buffer length.
+ */
+constexpr std::size_t RESULT_MESSAGE_SIZE = 128U;
+
+/*!
+ * \brief           An enum class representing all possible return values.
+ */
+enum class Result {
+    /*!
+     * \defgroup      CommonResultCodes Common Result Codes
+     * @{
+     */
+
+    SUCCESS,                 /*!< Everything's OK */
+    UNKNOWN_ERROR,           /*!< Unknown error */
+    INVALID_ARGUMENT,        /*!< Invalid argument */
+    WRONG_SIZE,              /*!< Wrong size */
+    UNEXPECTED_NULL_POINTER, /*!< Unexpected NULL pointer */
+
+    /*!
+     * @}
+     */
+
+    /*!
+      * \defgroup      FSMResultCodes FSM Result Codes
+      * @{
+      */
+
+    INVALID_NEXT_STATE, /*!< Invalid FSM next state */
+
+    /*!
+     * @}
+     */
+};
+
+/*!
+ * \brief           Set the error message.
+ *
+ * \param[in]       fmt Format string.
+ * \param[in]       ... variable arguments.
+ * \return          Result SUCCESS on success, an error code otherwise.
+ *                  - UNKNOWN_ERROR if an error occurs while.
+ */
+Result result_set_err_msg(const char *fmt, ...);
+
+/*!
+ * \brief           Get the error message.
+ *
+ * \return          The pointer to the error message.
+ */
+const char *result_get_err_msg(void);
+
+#endif /*! RESULT_H */
---
+++ firmware/src/result.cpp
@@ -0,0 +1,36 @@
+/*!
+ * \file            result.c
+ * \date            2025-12-19
+ * \author          Giulia Cristofolini [giulia.cristofolini@studenti.unitn.it]
+ *                  Mirko Lana [lana.mirko@icloud.com]
+ *                  Mattia Zagatti [mattia.zagatti@studenti.unitn.it]
+ *
+ * \brief           Return codes and error handling.
+ */
+
+#include "result.h"
+
+#include <cstdio>
+#include <cstdarg>
+#include <array>
+
+static std::array<char, RESULT_MESSAGE_SIZE> return_err_msg; /*!< The error message buffer */
+
+Result result_set_err_msg(const char *fmt, ...) {
+    if (!fmt)
+        return Result::INVALID_ARGUMENT;
+
+    std::va_list args;
+    va_start(args, fmt);
+
+    return_err_msg.fill('\0');
+    int len = vsnprintf(return_err_msg.data(), return_err_msg.size(), fmt, args);
+
+    va_end(args);
+
+    return len < 0 ? Result::UNKNOWN_ERROR : Result::SUCCESS;
+}
+
+const char *result_get_err_msg(void) {
+    return return_err_msg.data();
+}
